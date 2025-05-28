cbb430cd2f5ed714609911a3772ee100e44da648
sakthisudarshan <sakthisudarshan1@gmail.com>
2025-05-28 15:20:06 +0530

    developer code write

---
+++ Client/src/Pages/Developer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Developer(props) {
+    return (
+        <div>
+            <div className="bg-white min-h-screen w-full"></div>
+           <h1> Developer page</h1> 
+           <div class="photo1">
+               <div class="box1">
+                <li>Name</li>
+                <li>Degree</li>
+                <li>Team lead</li>
+               </div>
+               
+           </div>
+           </div>
+    );
+}
+
+export default Developer;

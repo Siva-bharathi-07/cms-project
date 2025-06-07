e10a9e01e8424be8a84d2c5b1541d4c15262feea
sakthisudarshan <sakthisudarshan1@gmail.com>
2025-06-07 23:37:14 +0530

    ff

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import './App.css'
-import Developer from './Pages/Developer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Developer from "./Pages/Developer";
+import Home from "./Pages/Home";
+import Assessment from "./Pages/assessment";
+import Ass from "./Pages/Ass";
 
-
-function App( ) {
+function App() {
   return (
-    <div>
-      <Developer></Developer>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Assessment" element={<Assessment />} />
+        <Route path="/Login" element={<Home />} />
+        <Route path="/Developer" element={<Developer />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;

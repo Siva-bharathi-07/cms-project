afc736ec25b21bd9a1afaec8e5ae85b6fdb36af8
Sivabharathi271 <improveknowlgde@gmail.com>
2025-06-21 21:34:27 +0530

    last

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,17 +14,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/assessment" element={<Assessment />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/developer" element={<Developer />} />
         <Route path="/ass" element={<Ass />} />
-
-        <Route path="/Assessment" element={<Assessment />} />
-        <Route path="/Login" element={<Home />} />
-        <Route path="/Ass" element={<Ass />} />
-        <Route path="/Developer" element={<Developer />} />
       </Routes>
     </Router>
   );
